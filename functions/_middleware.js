bf4ce4a826215ec5a3b2095f28cc3065f0ae2587
Kaprukadigitalmarketing <ifariakthar@gmail.com>
2026-06-19 11:25:35 +0530

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -42,7 +42,7 @@ function isValidUser(authHeader, env) {
     const [username, password] = decoded.split(':');
     
     const users = {
-      'aloka': env.ALOKA_PASS,
+      'B2BM': env.CORPORATE_PASS,
       'lahiru': env.LAHIRU_PASS,
       'ruwini': env.RUWINI_PASS,
       'iresha': env.IRESHA_PASS,
